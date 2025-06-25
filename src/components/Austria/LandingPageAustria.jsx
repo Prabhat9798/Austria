f73467d6aeb4b914c8b8d3618decdc0efe880e42
Prabhat <prabhat809221@gmail.com>
2025-06-25 15:45:19 +0530

    Commit10

--- src/components/Austria/LandingPageAustria.jsx
+++ src/components/Austria/LandingPageAustria.jsx
@@ -126,17 +126,17 @@ const LandingPageAustria = () => {
           <div className="lg:hidden flex flex-col items-center">
             <div className="w-full max-w-sm text-center mb-8">
               <h1 className="text-2xl md:text-3xl font-bold mb-6 leading-snug text-white">
-                Shape Your Future in <span className="bg-gradient-to-r from-[#E56D09] via-[#D83E13] to-[#D83E13] bg-clip-text text-transparent">AUSTRIA</span> 
+                Study & Shape Your Future in <span className="text-5xl bg-gradient-to-r from-[#E56D09] via-[#D83E13] to-[#D83E13] bg-clip-text text-transparent ml-2">AUSTRIA</span> 
               </h1>
               
               <div className="space-y-4 text-white">
                 {[
                   "100% Admission Guaranteed",
-                  "No IELTS required",
+                  "100% English taught courses for Masters and Bachelors",
                   "No APS required"
                 ].map((item, index) => (
                   <div key={index} className="flex items-center gap-3 justify-center">
-                    <div className="flex-shrink-0">
+                    <div className="flex-shrink-0 ">
                       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M21.3751 13.1246C20.6251 16.8746 17.7976 20.4056 13.8301 21.1946C11.8951 21.5799 9.88781 21.345 8.0941 20.5232C6.30039 19.7014 4.81167 18.3346 3.83993 16.6175C2.8682 14.9003 2.46297 12.9204 2.68196 10.9596C2.90094 8.99877 3.73298 7.15702 5.05959 5.69658C7.78059 2.69958 12.3751 1.87458 16.1251 3.37458" stroke="#00A745" strokeWidth="2.25" strokeLinecap="round" strokeLinejoin="round" />
                         <path d="M8.625 11.625L12.375 15.375L21.375 5.625" stroke="#00A745" strokeWidth="2.25" strokeLinecap="round" strokeLinejoin="round" />
@@ -252,18 +252,19 @@ const LandingPageAustria = () => {
           <div className="hidden lg:flex flex-col lg:flex-row items-center justify-center gap-8 lg:gap-12">
             {/* Left Content */}
             <div className="w-full max-w-2xl lg:w-[50%] text-white pl-2 lg:pl-6">
-              <h1 className="text-2xl md:text-4xl font-bold mb-6 leading-snug">
-                Shape Your Future in <span className="bg-gradient-to-r from-[#E56D09] via-[#D83E13] to-[#D83E13] bg-clip-text text-transparent">AUSTRIA</span> 
+              <h1 className="text-2xl md:text-4xl font-bold mb-6 leading-snug ">
+                Study & Shape your future in 
+ <span className="text-5xl bg-gradient-to-r from-[#E56D09] via-[#D83E13] to-[#D83E13] bg-clip-text text-transparent ml-4 ">AUSTRIA</span> 
               </h1>
               
               <div className="space-y-4">
                 {[
                   "100% Admission Guaranteed",
-                  "No IELTS required",
+                  "100% English taught courses for Masters and Bachelors",
                   "No APS required"
                 ].map((item, index) => (
                   <div key={index} className="flex items-center gap-3">
-                    <div className="flex-shrink-0">
+                    <div className="flex-shrink-0 flex-col">
                       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M21.3751 13.1246C20.6251 16.8746 17.7976 20.4056 13.8301 21.1946C11.8951 21.5799 9.88781 21.345 8.0941 20.5232C6.30039 19.7014 4.81167 18.3346 3.83993 16.6175C2.8682 14.9003 2.46297 12.9204 2.68196 10.9596C2.90094 8.99877 3.73298 7.15702 5.05959 5.69658C7.78059 2.69958 12.3751 1.87458 16.1251 3.37458" stroke="#00A745" strokeWidth="2.25" strokeLinecap="round" strokeLinejoin="round" />
                         <path d="M8.625 11.625L12.375 15.375L21.375 5.625" stroke="#00A745" strokeWidth="2.25" strokeLinecap="round" strokeLinejoin="round" />
