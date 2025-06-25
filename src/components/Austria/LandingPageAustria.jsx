71f30889c0e97fac7d22a52c17a885881dc889cd
Prabhat <prabhat809221@gmail.com>
2025-06-25 16:28:46 +0530

    Commit11

--- src/components/Austria/LandingPageAustria.jsx
+++ src/components/Austria/LandingPageAustria.jsx
@@ -130,21 +130,25 @@ const LandingPageAustria = () => {
               </h1>
               
               <div className="space-y-4 text-white">
-                {[
-                  "100% Admission Guaranteed",
-                  "100% English taught courses for Masters and Bachelors",
-                  "No APS required"
-                ].map((item, index) => (
-                  <div key={index} className="flex items-center gap-3 justify-center">
-                    <div className="flex-shrink-0 ">
-                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M21.3751 13.1246C20.6251 16.8746 17.7976 20.4056 13.8301 21.1946C11.8951 21.5799 9.88781 21.345 8.0941 20.5232C6.30039 19.7014 4.81167 18.3346 3.83993 16.6175C2.8682 14.9003 2.46297 12.9204 2.68196 10.9596C2.90094 8.99877 3.73298 7.15702 5.05959 5.69658C7.78059 2.69958 12.3751 1.87458 16.1251 3.37458" stroke="#00A745" strokeWidth="2.25" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M8.625 11.625L12.375 15.375L21.375 5.625" stroke="#00A745" strokeWidth="2.25" strokeLinecap="round" strokeLinejoin="round" />
-                      </svg>
-                    </div>
-                    <p className="text-lg md:text-xl font-medium">{item}</p>
-                  </div>
-                ))}
+              {[
+        "100% Admission Guaranteed",
+        <span key="english-courses">
+          100% English taught courses<br />for Masters and Bachelors
+        </span>,
+        "No APS required"
+      ].map((item, index) => (
+        <div key={index} className="flex gap-3 items-start">
+          <div className="flex-shrink-0 pt-1">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M21.3751 13.1246C20.6251 16.8746 17.7976 20.4056 13.8301 21.1946C11.8951 21.5799 9.88781 21.345 8.0941 20.5232C6.30039 19.7014 4.81167 18.3346 3.83993 16.6175C2.8682 14.9003 2.46297 12.9204 2.68196 10.9596C2.90094 8.99877 3.73298 7.15702 5.05959 5.69658C7.78059 2.69958 12.3751 1.87458 16.1251 3.37458" stroke="#00A745" strokeWidth="2.25" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M8.625 11.625L12.375 15.375L21.375 5.625" stroke="#00A745" strokeWidth="2.25" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+          </div>
+          <p className="text-lg md:text-xl font-medium">
+            {item}
+          </p>
+        </div>
+      ))}
               </div>
             </div>
 
